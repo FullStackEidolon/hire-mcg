f2746154018e452271f00cdc9a980ec303a6851d
Ian McGahren <60856620+FullStackEidolon@users.noreply.github.com>
2023-06-08 04:04:31 -0400

    V 1.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-
+import resume from './Ian_McGahren_Resume.pdf'
 function App() {
   return (
     <div id="wrapper">
@@ -13,8 +13,8 @@ function App() {
 						<p>Software Engineer &nbsp;&bull;&nbsp; Property Manager &nbsp;&bull;&nbsp; Musician</p>
 						<nav>
 							<ul>
-								<li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li>
 								<li><a href="https://github.com/FullStackEidolon" className="icon brands fa-github"><span className="label">Github</span></a></li>
+								<li><a href={resume} className="icon solid fa-file-alt" download><span className="label" >Dribbble</span></a></li>
 								<li><a href="mailto:mcgahreni@gmail.com" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
 							</ul>
 						</nav>
